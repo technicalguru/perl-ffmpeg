974071155acca5f2e3f05968b06d0663842bd7f7
Ralph Schuster <github@ralph-schuster.eu>
2015-06-15 13:36:45 +0000

    Conversion computation

--- Ffmpeg.pm
+++ Ffmpeg.pm
@@ -4,6 +4,36 @@ use strict;
 my $FFMPEG_ROOT = '/usr/local/ffmpeg';
 my $FFMPEG_BIN  = "$FFMPEG_ROOT/ffmpeg";
 my $FFPROBE_BIN = "$FFMPEG_ROOT/ffprobe";
+my $ENCODERS = {
+	# Video
+	'ffv' => 'ffv1',
+	'flv' => 'flv',
+	'h261' => 'h261',
+	'h263' => 'h263',
+	'h264' => 'libx264',
+	'h265' => 'libx265',
+	'mpeg1video' => 'mpeg1video',
+	'mpeg2video' => 'mpeg2video',
+	'mpeg4' => 'libxvid',
+	'wmv' => 'wmv2',
+	# Audio
+	'aac' => 'libvo_aacenc',
+	'ac3' => 'ac3_fixed',
+	'flac' => 'flac',
+	'mp2' => 'mp2',
+	'mp3' => 'libmp3lame',
+	'vorbis' => 'libvorbis',
+	'wma' => 'wmav2',
+	# Subtitles
+	'dvb_subtitle' => 'dvbsub',
+	'dvd_subtitle' => 'dvdsub',
+	'mov_text' => 'mov_text',
+	'ssa' => 'ssa',
+	'srt' => 'srt',
+	'subrip' => 'subrip',
+	'webvtt' => 'webvtt',
+	'xsub' => 'xsub',
+};
 
 sub new {
 	my $self = {};
@@ -19,7 +49,7 @@ sub new {
 #    'audio' => [
 #        <stream-id>* => [ <audio-description> ]
 #    ],
-#    'subtitles' => [
+#    'subtitle' => [
 #        <stream-id>* => [ <subtitles-description> ]
 #    ]
 # ]
@@ -34,7 +64,7 @@ sub analyze {
 	my $rc = {
 		'video' => {},
 		'audio' => {},
-		'subtitles' => {},
+		'subtitle' => {},
 	};
 
 	my $cmd = "$FFPROBE_BIN -v quiet -print_format xml -show_format -show_streams $file";
@@ -53,6 +83,10 @@ sub analyze {
 				$rc->{$streamType}->{$streamId}->{languageCode} = $1;
 			} elsif ($line =~ /^\s*<disposition default="(\d)"/i) {
 				$rc->{$streamType}->{$streamId}->{default} = $1;
+			} elsif ($line =~ /^\s*<format\s+/i) {
+				$rc->{format} = $self->parseAttributes($line);
+				$rc->{filename} = $rc->{format}->{filename};
+				$streamType = 'format'; # will put all other attributes there
 			}
 
 		}
@@ -76,8 +110,8 @@ sub getLongSummary {
 	foreach $stream (sort(keys(%{$desc->{audio}}))) {
 		push(@RC, $self->getStreamString($desc->{audio}->{$stream}));
 	}
-	foreach $stream (sort(keys(%{$desc->{subtitles}}))) {
-		push(@RC, $self->getStreamString($desc->{subtitles}->{$stream}));
+	foreach $stream (sort(keys(%{$desc->{subtitle}}))) {
+		push(@RC, $self->getStreamString($desc->{subtitle}->{$stream}));
 	}
 	return \@RC;
 }
@@ -105,11 +139,11 @@ sub getShortSummary {
 		push(@AUDIO, "$lang (".$desc->{audio}->{$stream}->{codec_name}.")");
 	}
 	my @SUBTITLES = ();
-	foreach $stream (keys(%{$desc->{subtitles}})) {
-		my $lang = $desc->{subtitles}->{$stream}->{'languageCode'};
-		$lang = $desc->{subtitles}->{$stream}->{'languageId'} if !$lang;
+	foreach $stream (keys(%{$desc->{subtitle}})) {
+		my $lang = $desc->{subtitle}->{$stream}->{'languageCode'};
+		$lang = $desc->{subtitle}->{$stream}->{'languageId'} if !$lang;
 		$lang = 'unknown' if !$lang;
-		push(@SUBTITLES, "$lang (".$desc->{subtitles}->{$stream}->{codec_name}.")");
+		push(@SUBTITLES, "$lang (".$desc->{subtitle}->{$stream}->{codec_name}.")");
 	}
 
 	my $rc = "Video: ".join(', ', @VIDEO)." / Audio: ".join(', ', @AUDIO);
@@ -159,12 +193,18 @@ sub applyAppleFormats {
 	my $self = shift;
 	my $fromDescription = shift;
 
-	return $self->applyFormats(
-		$fromDescription.
+	my $rc = $self->applyFormats(
+		$fromDescription,
 		$self->getAppleVideo(),
 		$self->getAppleAudio(),
 		$self->getAppleSubtitles()
 	);
+
+	if ($rc->{filename} !~ /\.mp4/i) {
+		$rc->{filename} =~ s/\.[a-z0-9]+$/\.mp4/i;
+	}
+
+	return $rc;
 }
 
 # Returns standard iOS video codec: MPEG4
@@ -206,7 +246,8 @@ sub isAppleCompatible {
 	my $self = shift;
 	my $desc = shift;
 
-	return $self->isCompatible($desc, $self->getAppleVideo(), $self->getAppleAudio(), $self->getAppleSubtitles());
+	return $self->isCompatible($desc, $self->getAppleVideo(), $self->getAppleAudio(), $self->getAppleSubtitles()) &&
+		($desc->{format}->{format_name} eq 'mov,mp4,m4a,3gp,3g2,mj2');
 }
 
 # Applies codecs for Enigma.
@@ -217,7 +258,7 @@ sub applyEnigmaFormats {
 	my $fromDescription = shift;
 
 	return $self->applyFormats(
-		$fromDescription.
+		$fromDescription,
 		$self->getEnigmaVideo(),
 		$self->getEnigmaAudio(),
 		$self->getEnigmaSubtitles()
@@ -244,6 +285,7 @@ sub getEnigmaAudio {
 
 	my @RC = (
 		{ 'codec' => 'ac3' },
+		{ 'codec' => 'dts' },
 		{ 'codec' => 'aac' },
 		{ 'codec' => 'mp2' },
 		{ 'codec' => 'wav' },
@@ -392,9 +434,86 @@ sub applyFormats {
 	my $videoFormat = shift;
 	my $audioFormat = shift;
 	my $subtitlesFormat = shift;
-	my $rc = [];
+
+	# Make a complete copy before adjusting codecs
+	my $rc = $self->copyHash($description);
+	$self->applyBestCodec($rc->{video}, $videoFormat) if !$self->isVideoCompatible($description, $videoFormat);
+	$self->applyBestCodec($rc->{audio}, $audioFormat) if !$self->isAudioCompatible($description, $audioFormat);
+	$self->applyBestCodec($rc->{subtitle}, $subtitlesFormat) if !$self->isSubtitleCompatible($description, $subtitlesFormat);
+	return $rc;
+}
+
+# Applies the best codec onto the given description.
+# The new codecs are given in order of priority/quality. That means that a codec is replaced only when 
+# existing codec is not mentioned as new codec. Otherwise the codec will be replaced.
+# @param $desc   - hashref  - description of stream (video, audio, subtitle)
+# @param $codecs - arrayref - codecs in order of priority
+# @return NIL - $desc will be modified properly
+sub applyBestCodec {
+	my $self = shift;
+	my $desc = shift;
+	my $codecs = shift;
+
+	my ($stream, $key, $codec, $bestCodec);
+	foreach $stream (keys(%{$desc})) {
+		my $found = 0;
+		foreach $codec (@{$codecs}) {
+			$bestCodec = $codec if !$bestCodec;
+			if ($desc->{$stream}->{codec_name} eq $codec->{codec}) {
+				if (!$codec->{profile} || ($codec->{profile} eq $desc->{$stream}->{profile})) {
+					$found = 1;
+					last;
+				}
+			}
+		}
+		if (!$found) {
+			$desc->{$stream}->{codec_name} = $bestCodec->{codec};
+			$desc->{$stream}->{profile} = $bestCodec->{profile};
+		}
+	}
+}
+
+# copies a hash.
+sub copyHash {
+	my $self = shift;
+	my $orig = shift;
+	my $rc = {};
+	my $key;
+
+	foreach $key (keys(%{$orig})) {
+		my $value = $orig->{$key};
+		my $type = ref($value);
+		if ($type eq 'HASH') {
+			$rc->{$key} = $self->copyHash($value);
+		} elsif ($type eq 'ARRAY') {
+			$rc->{$key} = $self->copyArray($value);
+		} else {
+			$rc->{$key} = $value;
+		}
+	}
 
 	return $rc;
+}
+
+# Copies an array
+sub copyArray {
+	my $self = shift;
+	my $orig = shift;
+	my @RC = ();
+	my $value;
+
+	foreach $value (@{$orig}) {
+		my $type = ref($value);
+		if ($type eq 'HASH') {
+			push(@RC, $self->copyHash($value));
+		} elsif ($type eq 'ARRAY') {
+			push(@RC, $self->copyArray($value));
+		} else {
+			push(@RC, $value);
+		}
+	}
+
+	return \@RC;
 }
 
 # Computes the correct conversion for Apple.
@@ -437,42 +556,226 @@ sub getEnigmaConversion {
 # @param $targetDescription - hashref - description of destination, see analyze function
 # @param $options           - hashref - describes parameters (optional)
 #         [
-#            'defaultVideo' => <streamid>, (default is first video stream)
+#            'defaultVideo' => <streamid>, (no default)
 #            'defaultAudio' => <streamid> or <language> (ger/deu/eng/de/en - default is first audio stream),
 #         ]
 # @returns arrayref - see convert function
 sub getConversion {
-	my $self = shift;
+	my $self     = shift;
 	my $fromDescription = shift;
 	my $targetDescription = shift;
+	my $options  = shift;
+	my @RC       = ();
+	my @VIDEO    = ();
+	my @AUDIO    = ();
+	my @SUBTITLE = ();
+	my ($stream);
+
+	# video streams
+	foreach $stream (keys(%{$fromDescription->{video}})) {
+		my $srcStream = $fromDescription->{video}->{$stream};
+		my $dstStream = exists $targetDescription->{video}->{$stream} ? $targetDescription->{video}->{$stream} : 0;
+		if ($dstStream) {
+			my $convert = $self->getVideoConversion($srcStream, $dstStream, $options);
+
+			# Add stream
+			$self->addConversion(\@VIDEO, $convert);
+		}
+	}
+
+	# audio streams
+	foreach $stream (keys(%{$fromDescription->{audio}})) {
+		my $srcStream = $fromDescription->{audio}->{$stream};
+		my $dstStream = exists $targetDescription->{audio}->{$stream} ? $targetDescription->{audio}->{$stream} : 0;
+		if ($dstStream) {
+			# Detect conversion
+			my $convert = $self->getAudioConversion($srcStream, $dstStream, $options);
+
+			# Add stream
+			$self->addConversion(\@AUDIO, $convert);
+		}
+	}
+
+	# subtitle streams
+	foreach $stream (keys(%{$fromDescription->{subtitle}})) {
+		my $srcStream = $fromDescription->{subtitle}->{$stream};
+		my $dstStream = exists $targetDescription->{subtitle}->{$stream} ? $targetDescription->{subtitle}->{$stream} : 0;
+		if ($dstStream) {
+			# Detect conversion
+			my $convert = $self->getSubtitleConversion($srcStream, $dstStream, $options);
+
+			# Add stream
+			$self->addConversion(\@SUBTITLE, $convert);
+		}
+	}
+
+	push(@RC, @VIDEO);
+	push(@RC, @AUDIO);
+	push(@RC, @SUBTITLE);
+	return \@RC;
+}
+
+# Determines the correct video conversion between two stream descriptions.
+# @param $srcStream - hashref - description of source video stream
+# @param $dstStream - hashref - description of destination video stream
+# @param $options   - hashref - additional options
+# @returns arrayref - description of conversion
+sub getVideoConversion {
+	my $self = shift;
+	my $srcStream = shift;
+	my $dstStream = shift;
 	my $options = shift;
+
+	my $convert = {
+		'streamId'   => '0:'.$srcStream->{index},
+		'streamType' => 'video',
+		'language'   => $srcStream->{languageCode},
+		'options'    => [],
+		'default'    => 0,
+	};
+
+	# Detect conversion type
+	if ($srcStream->{codec_name} eq $dstStream->{codec_name}) {
+		$convert->{codec} = 'copy';
+	} else {
+		$convert->{codec} = $self->getCodecLib($dstStream->{codec_name});
+	}
+
+	# Some more options
+
+	if (($convert->{streamId} eq $options->{defaultVideo}) || ($convert->{language} eq $options->{defaultVideo})) {
+		$convert->{default} = 1;
+	}
+	return $convert;
+}
+
+# Determines the correct audio conversion between two stream descriptions.
+# @param $srcStream - hashref - description of source audio stream
+# @param $dstStream - hashref - description of destination audio stream
+# @param $options   - hashref - additional options
+# @returns arrayref - description of conversion
+sub getAudioConversion {
+	my $self = shift;
+	my $srcStream = shift;
+	my $dstStream = shift;
+	my $options = shift;
+
+	my $convert = {
+		'streamId'   => '0:'.$srcStream->{index},
+		'streamType' => 'audio',
+		'language'   => $srcStream->{languageCode},
+		'options'    => [],
+		'default'    => 0,
+	};
+
+	# Detect conversion type
+	if ($srcStream->{codec_name} eq $dstStream->{codec_name}) {
+		$convert->{codec} = 'copy';
+	} else {
+		$convert->{codec} = $self->getCodecLib($dstStream->{codec_name});
+	}
+
+	# Some more options
+
+	if (($convert->{streamId} eq $options->{defaultAudio}) || ($convert->{language} eq $options->{defaultAudio})) {
+		$convert->{default} = 1;
+	}
+	return $convert;
+}
+
+# Determines the correct subtitle conversion between two stream descriptions.
+# @param $srcStream - hashref - description of source subtitle stream
+# @param $dstStream - hashref - description of destination subtitle stream
+# @param $options   - hashref - additional options
+# @returns arrayref - description of conversion
+sub getSubtitleConversion {
+	my $self = shift;
+	my $srcStream = shift;
+	my $dstStream = shift;
+	my $options = shift;
+
+	my $convert = {
+		'streamId'   => '0:'.$srcStream->{index},
+		'streamType' => 'subtitle',
+		'language'   => $srcStream->{languageCode},
+		'options'    => [],
+		'default'    => 0,
+	};
+
+	# Detect conversion type
+	if ($srcStream->{codec_name} eq $dstStream->{codec_name}) {
+		$convert->{codec} = 'copy';
+	} else {
+		$convert->{codec} = $self->getCodecLib($dstStream->{codec_name});
+	}
+
+	# Some more options
+
+	if (($convert->{streamId} eq $options->{defaultSubtitle}) || ($convert->{language} eq $options->{defaultSubtitle})) {
+		$convert->{default} = 1;
+	}
+	return $convert;
+}
+
+# Adds the description to the array depending whether it is the default stream or not.
+# @param $arr    - arrayref - return parameter
+# @param $desc   - hashref  - description to be added to return parameter
+# @param default - string   - streamId or languageCode of default description.
+sub addConversion {
+	my $self = shift;
+	my $arr  = shift;
+	my $desc = shift;
+
+
+	if ($desc->{default}) {
+		unshift(@{$arr}, $desc);
+	} else {
+		push(@{$arr}, $desc);
+	}
+}
+
+# Returns the correct encoding parameter for a specific codec
+sub getCodecLib {
+	my $self = shift;
+	my $codecName = shift;
+
+	if ($ENCODERS->{$codecName}) {
+		return $ENCODERS->{$codecName};
+	}
+	return $codecName;
+}
+
+# Build the ffmpeg conversion command from the conversion description
+# @param $desc - hashref - the conversion description (see convert function)
+# @returns array - parameters to ffmpeg binary
+sub getConvertCommand {
+	my $self = shift;
+	my $desc = shift;
+	# -metadata:s:a:0 language=eng
 }
 
 # Converts the given file.
 # @param $srcPath - string - path of source file
 # @param $options - arrayref - description of conversion
 # [
-#    0 => [
-#        <stream-id>* => [
-#            'codec' => <codec>,
-#            'options' => [
-#            ],
-#        ],
-#    ],
-#    1 => [
-#        <stream-id>* => [
-#            'codec' => <codec>,
-#            'options' => [
-#            ],
-#        ],
-#    ],
-#    2 => [
-#        <stream-id>* => [
-#            'codec' => <codec>,
-#            'options' => [
-#            ],
-#        ],
-#    ]
+#    0 => {
+#        'streamId' => <stream-id>,
+#        'streamType' => 'video'|'audio'|'subtitle',
+#        'codec' => <codec>,
+#        'language' => <language>,
+#        'default' => 1|0,
+#        'options' => {
+#        },
+#    },
+#    1 => {
+#        'streamId' => <stream-id>,
+#        'streamType' => 'video'|'audio'|'subtitle',
+#        'codec' => <codec>,
+#        'language' => <language>,
+#        'default' => 1|0,
+#        'options' => {
+#        },
+#    },
 # ]
 # @param $dstPath - string - path of destination file (optional, default is to replace source file)
 # @return 1 if successful
